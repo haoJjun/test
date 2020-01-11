8018df0107abfbbcac17f138d9cf7286cefcd474
haozhijun <451211756@qq.com>
2020-01-11 22:41:40 +0800

    首页

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,8 @@
 <template>
-  <div id="app">
+  <!-- 一级路由容器放一级路由组件 -->
     <router-view/>
-  </div>
 </template>
 
 <style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="login">
+        <!-- element -->
+        <!-- 卡片组件 -->
+        <el-card class="login-card">
+            <!-- 卡片内容 -->
+            <div class="title">
+                <img src="../../assets/img/logo_index.png" alt="">
+            </div>
+            <!-- 表单 -->
+            <el-form style="margin-top:30px">
+                <el-form-item >
+                    <!-- 手机号 -->
+                    <el-input></el-input>
+                </el-form-item>
+                <el-form-item >
+                    <!-- 验证码 -->
+                    <el-input style="width:280px"></el-input>
+                    <!-- 按钮 -->
+                    <el-button plain style="float:right">发送验证码</el-button>
+                </el-form-item>
+                <el-form-item>
+                    <!-- 勾选同意框 -->
+                    <el-checkbox>我已阅读并同意协议</el-checkbox>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" style="width:100%">登录</el-button>
+                </el-form-item>
+            </el-form>
+        </el-card>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.login {
+    background: url('../../assets/img/login_bg.jpg');
+    background-size: cover;
+    height:100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .login-card {
+        width:440px ;
+        height: 360px;
+        .title {
+            text-align: center;
+            img {
+                height: 45px;
+            }
+        }
+    }
+}
+</style>
+>
